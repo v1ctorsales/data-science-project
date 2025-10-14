54a37d6852b3bde28ccca3efcc7b049972839a5d
Victor Sales <36749678+v1ctorsales@users.noreply.github.com>
2025-10-14 21:58:45 +0300

    chore: initial project structure setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def main():
+    array = np.array([1, 2, 3, 4, 5])
+
+    arrsum = np.sum(array)
+
+    print(f"Array: {array}")
+    print(f"Sum: {arrsum}")
+
+if __name__ == "__main__":
+    main()
